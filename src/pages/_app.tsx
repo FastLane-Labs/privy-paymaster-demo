550474e8c45927f7900d7c29e34406dc4488f637
0xNotMe <nme15500@gmail.com>
2025-03-01 14:49:57 +1100

    fix: privy smart wallet creation setting to all-users

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       config={{
         loginMethods: ['email', 'wallet'],
         embeddedWallets: {
-          createOnLogin: 'users-without-wallets',
+          createOnLogin: 'all-users',
         },
         appearance: {
           theme: 'light',
